73cb049043cc3334eeece0af1d7d9fab28093f77
Joost Steltenpool <jcsteltenpool@users.noreply.github.com>
2023-07-12 17:00:05 +0200

    update projectdata

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -26,15 +26,20 @@ export const projectsData = [{
         },
         features: ['Gebouwd in Elementor', 'Aangevuld met custom css, html en javascript', 'Fully responsive']
     },
-    // {
-    //     title: 'Theaterbazen',
-    //     siteUrl: 'https://theaterbazen.nl',
-    //     description: 'Theaterbazen is een project van NEOS Cultuuronderwijs. Op de site vind je onder andere een zesdelige online serie en een overzicht van plekken in Amersfoort waar je (jeugd)theater kunt zien en spelen.',
-    //     images: {
-    //         movie: '/images/theaterbazen_mobile_short.mp4',
-    //         mobile: '/images/theaterbazen_mobile.webp',
-    //         desktop: '/images/theaterbazen_desktop.webp'
-    //     },
-    //     features: ['Gebouwd in Elementor', 'Aangevuld met custom css, html en javascript', 'Fully responsive']
-    // }
+    {
+        title: 'Functional Jammming',
+        siteUrl: 'https://functional-jammming.netlify.app/',
+        codeUrl: 'https://github.com/jcsteltenpool/functional_jammming',
+        description: 'Een Codecademy project waarmee in de browser verbinding gemaakt wordt met het Spotify-account van de gebruiker. Zo kunnen er nieuwe playlists worden samengesteld en opgeslagen in het account.',
+        images: {
+            mobile: '/images/functional_jammming_mobile.webp',
+            desktop: '/images/functional_jammming_desktop.webp'
+        },
+        features: [
+            'Gebouwd in React',
+            'Verbinding met Spotify API: OAuth 2.0 en Fetch API',
+            'Van class components naar function components', 
+            'Extra feature ingebouwd: preview tracks luisteren en de voortgang van een animatie voorzien'
+        ]
+    }
 ]
